189248b793239b14c66c579788ccd1aef24d0d99
Keith Baker <syzygy@specialks.com>
2025-06-20 22:58:20 -0400

    WTF? version pin seems broken

--- esphome/components/nmea2k/__init__.py
+++ esphome/components/nmea2k/__init__.py
@@ -69,7 +69,7 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     cg.add_library(
         name="NMEA2000",
-        repository="ttlappalainen/NMEA2000-library",
+        repository="ttlappalainen/NMEA2000-library#v4.21.5",
         version="4.21.5",
     )
     can_tx_pin = await cg.gpio_pin_expression(config[CONF_CAN_TX_PIN])
