2b9cfadfe8e6fcdadc5a179c9e06502f23c84617
Keith Baker <syzygy@specialks.com>
2025-06-20 22:32:49 -0400

    include nmea2000 library through config

--- esphome/components/nmea2k/__init__.py
+++ esphome/components/nmea2k/__init__.py
@@ -67,6 +67,11 @@ CONFIG_SCHEMA = (
 )
 
 async def to_code(config):
+    cg.add_library(
+        name="NMEA2000",
+        repository="https://github.com/ttlappalainen/NMEA2000-library.git",
+        version=None,
+    )
     can_tx_pin = await cg.gpio_pin_expression(config[CONF_CAN_TX_PIN])
     can_rx_pin = await cg.gpio_pin_expression(config[CONF_CAN_RX_PIN])
 
