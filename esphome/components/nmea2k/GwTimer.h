ac66b7000e4974f59bbbb18abd95c2665c5036ca
Keith Baker <syzygy@specialks.com>
2025-06-20 23:38:28 -0400

    limited import to maybe stop circular?

--- esphome/components/nmea2k/GwTimer.h
+++ esphome/components/nmea2k/GwTimer.h
@@ -2,7 +2,8 @@
 #define _GWTIMER_H
 #include <vector>
 #include <functional>
-#include "esphome.h"
+#include "esphome/core/helpers.h" #more specifically for esphome::millis()
+//#include "esphome.h"
 // #include <Arduino.h>
 class GwIntervalRunner{
     public:
